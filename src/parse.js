edf867eb77d6a4d2c0904a447c1fd0f596f41bdc
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2020-03-09 13:25:51 +0200

    exercise done

--- src/parse.js
+++ src/parse.js
@@ -1,13 +1,38 @@
+'use strict';
+
 const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
 
 const parenthesize = tokens => {
-  return tokens;
+  const token = pop(tokens);
+
+  if (isOpeningParenthesis(token.value)) {
+    const expression = [];
+
+    while (!isClosingParenthesis(peek(tokens).value)) {
+      expression.push(parenthesize(tokens));
+    }
+
+    tokens.pop();
+    return expression;
+  }
+
+  return token;
 };
 
 const parse = tokens => {
-  const token = pop(tokens);
+  if (Array.isArray(tokens)) {
+    const [first, ...rest] = tokens;
+
+    return {
+      type: 'CallExpression',
+      name: first.value,
+      arguments: rest.map(parse)
+    };
+  }
+
+  const token = tokens;
 
   if (token.type === 'Number') {
     return {
@@ -15,6 +40,42 @@ const parse = tokens => {
       value: token.value,
     };
   }
+
+  if (token.type === 'String') {
+    return {
+      type: 'StringLiteral',
+      value: token.value,
+    };
+  }
+
+  if (token.type === 'Name') {
+    return {
+      type: 'Identifier',
+      name: token.value,
+    };
+  }
+
+  // My version
+  /*/
+  if (token.type === 'Parenthesis' && isOpeningParenthesis(token.value)) {
+    const ast = {
+      type: 'CallExpression'
+    };
+
+    if (peek(tokens).type === 'Name') {
+      ast.name = peek(tokens).value;
+      ast.arguments = [];
+      pop(tokens);
+    }
+
+    while (!(peek(tokens).type === 'Parenthesis' && isClosingParenthesis(peek(tokens).value))) {
+      ast.arguments.push(parse(tokens));
+    }
+
+    return ast;
+  }
+  //*/
+
 };
 
 module.exports = { parse: tokens => parse(parenthesize(tokens)) };
